5d049db59012c14d37e63af93626b8416cebfc1f
dharmit-addweb <dharmit.addweb@gmail.com>
2018-10-09 18:41:28 +0530

    change the css and html file

--- src/js/pages/theme.js
+++ src/js/pages/theme.js
@@ -1,12 +1,5 @@
-// const $ = require('jquery');
-// window.$ = window.jQuery = require('jquery');
-// window.$ = window.jQuery = require('jquery');
-const popper = require('popper.js');
-const bootstrap = require('bootstrap');
-const headerInit = require('./../sections/header');
-	
-headerInit();
 
-module.exports = () => {    
-    
-};
+jQuery(document).ready(function(){
+  alert('hi');
+  jQuery('#basic').selectric();
+});
